bf5714a44cfc5ec047767fed1b9545a7b90cd60e
9b2n <rbqls1057@gmail.com>
2025-04-21 01:45:42 +0900

    modify: user entity 수정 및 builder 추가

--- backend/src/main/java/com/wanted/backend/entity/User.java
+++ backend/src/main/java/com/wanted/backend/entity/User.java
@@ -1,6 +1,39 @@
+package com.wanted.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+    private String email;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Builder
+    public User(String name, String email, String avatarUrl, LocalDateTime createdAt) {
+        this.name = name;
+        this.email = email;
+        this.avatarUrl = avatarUrl;
+        this.createdAt = createdAt;
+    }
 }
